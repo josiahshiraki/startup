06b07b9b9705767ac609370f72fcb54c35da8799
josiahshiraki <josiahshiraki@gmail.com>
2026-02-12 10:47:22 -1000

    finish .jsx files, all routing completed

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import './login.css'
+
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
-    </main>
+  <main>
+    <section>
+      <h2>Login</h2>
+
+      <form method="get" action="home_page.html">
+
+        <div>
+          <label>
+            <span>@</span>
+            <input type="text" placeholder="your@email.com" />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            <span>🔒</span>
+            <input type="password" placeholder="password" />
+          </label>
+        </div>
+
+        <div>
+          <button type="submit">Login</button>
+          <button type="submit">Create Account</button>
+        </div>
+
+      </form>
+    </section>
+
+  </main>
   );
 }
